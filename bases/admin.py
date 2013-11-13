f00e46c53ee8852ae906257a63892fc7edb05df8
marcelogracia <marcelogracia@hotmail.com>
2013-11-12 23:18:52 -0300

    Nuevas definiciones de los modelos

--- bases/admin.py
+++ bases/admin.py
@@ -4,6 +4,9 @@ from models import Presupuestos, PlanDeCuentas, Proveedores
 class PresupuestosAdmin(admin.ModelAdmin):
 	list_display = ('anio', 'monto', 'ordenanza',)
 	# muestra los campos divididos por columnas en la grilla
-	
+
+class ProveedoresAdmin(admin.ModelAdmin):
+	list_display = ('razonsocial','cuit','direccion','tel_fijo','email')	
 
 admin.site.register(Presupuestos,PresupuestosAdmin)
+admin.site.register(Proveedores,ProveedoresAdmin)
--- bases/models.py
+++ bases/models.py
@@ -4,19 +4,19 @@ class Presupuestos(models.Model):
 	anio = models.DateField()
 	ordenanza = models.CharField(max_length=50)
 	thumb = models.ImageField(upload_to='/static/')
-	monto = models.FloatField(max_digits=15, decimal_places=2, null=False)
+	monto = models.FloatField()
 
 class PlanDeCuentas(models.Model):
 	nombrecuenta = models.CharField(max_length=180)
 	cuentapadre = models.IntegerField(null=True)
-	monto = models.FloatField(max_digits=15, decimal_places=2, null=False)
-	presupuesto = models.ForeignKey(Presupuesto)
+	monto = models.FloatField()
+	presupuesto = models.ForeignKey(Presupuestos)
 	thumb = models.ImageField(upload_to='/static/')
 
 class Proveedores(models.Model):
-	razonsocial = models.CharField(max_length=140 null=False blank=False)
-	cuit = models.CharField(max_length=11 null=False blank=False)
-	direccion = models.CharField(max_length=140 null=False blank=False)
+	razonsocial = models.CharField(max_length=140, null=False, blank=False)
+	cuit = models.CharField(max_length=11, null=False, blank=False)
+	direccion = models.CharField(max_length=140, null=False, blank=False )
 	tel_fijo = models.CharField(max_length=15)
 	tel_cel = models.CharField(max_length=15)
 	email = models.EmailField(max_length=254)
