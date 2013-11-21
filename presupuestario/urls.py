74d75d6478d247f331d9bb97db5da0b76546140e
marcelogracia <marcelogracia@hotmail.com>
2013-11-21 18:17:21 -0300

    agregue diseño del login. Hay que verlo y acomodar colores

--- presupuestario/urls.py
+++ presupuestario/urls.py
@@ -10,17 +10,14 @@ urlpatterns = patterns('',
 	# Examples:
     # url(r'^$', 'presupuestario.views.home', name='home'),
     # url(r'^presupuestario/', include('presupuestario.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^$', login.as_view() , name='login'),
     #url(r'^$', include(bases.urls)),
     url(r'^$', 'django.contrib.auth.views.login',
     	{'template_name':'inicio/index.html'}, name='login'),
-    url(r'^cerrar/$', 'django.contrib.auth.views.logout_then_login',
-    	name='logout'),
+    url(r'^cerrar/$', 'django.contrib.auth.views.logout_then_login',name='logout'),
     
 )
